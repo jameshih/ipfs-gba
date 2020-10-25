7fb2da3a297fc95edfce4a43cdeb7665214f3263
jameshih <j@shih.app>
2020-10-25 15:42:06 -0400

    removed return since function results are processed within callback, renamed cb => callback for better readability

--- frontend/src/utils/ipfs.ts
+++ frontend/src/utils/ipfs.ts
@@ -1,7 +1,7 @@
 import IPFS from 'ipfs-http-client';
 export const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
-export async function loadRomFromIPFS(hash: string, cb: Function) {
+export async function loadRomFromIPFS(hash: string, callback: Function) {
     const ipfs_data = [];
     for await (const data_slice of ipfs.cat(hash)) {
         for (const elm of data_slice) {
@@ -9,5 +9,5 @@ export async function loadRomFromIPFS(hash: string, cb: Function) {
         }
     }
     const file = new Blob([new Uint8Array(ipfs_data).buffer]);
-    return cb(file);
+    callback(file);
 }
--- frontend/src/utils/web3.ts
+++ frontend/src/utils/web3.ts
@@ -5,24 +5,24 @@ const ierc721metaABI = require('../constant/abi/IERC721Metadata.json');
 
 const web3 = new Web3(`https://rinkeby.infura.io/v3/${INFURA_KEY}`);
 
-export const balanceOf = async (tokenAddress: string, owner: string | null, cb: Function) => {
+export const balanceOf = async (tokenAddress: string, owner: string | null, callback: Function) => {
     const ierc721meta = new web3.eth.Contract(ierc721metaABI.abi, tokenAddress);
 
     const res = await ierc721meta.methods.balanceOf(owner).call();
-    return cb(res);
+    callback(res);
 };
 
-export const getDetail = async (tokenAddress: string, cb: Function) => {
+export const getDetail = async (tokenAddress: string, callback: Function) => {
     const ierc721meta = new web3.eth.Contract(ierc721metaABI.abi, tokenAddress);
 
     const name = await ierc721meta.methods.name().call();
     const symbol = await ierc721meta.methods.symbol().call();
-    return cb(name, symbol);
+    callback(name, symbol);
 };
 
-export const getTokenURI = async (tokenAddress: string, tokenID: number, cb: Function) => {
+export const getTokenURI = async (tokenAddress: string, tokenID: number, callback: Function) => {
     const ierc721meta = new web3.eth.Contract(ierc721metaABI.abi, tokenAddress);
 
     const res = await ierc721meta.methods.tokenURI(tokenID).call();
-    return cb(res);
+    callback(res);
 };
